565c5c2e27716273f9490e5bf8584ddf1aca014d
AncorGG <ancorgarcia63@gmail.com>
2025-08-18 12:39:15 +0100

    Main Page Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+
+      <div className="next-s-container">
+        <div className="next-s-header">
+          <h2>Next Session</h2>
+          <i>Admin Icon</i>
+        </div>
+        <div className="next-s-content">
+          <div className="next-s-content-item">
+            <div className="next-s-content-item-header">
+              <i>Icon</i>
+              <h3>Date</h3>
+            </div>
+            <p>Thu, Sept 15</p>
+          </div>
+          <div className="next-s-content-item">
+            <div className="next-s-content-item-header">
+              <i>Icon</i>
+              <h3>Time</h3>
+            </div>
+            <p>16:00 - 21:00</p>
+          </div>
+          <div className="next-s-content-item">
+            <div className="next-s-content-item-header">
+              <i>Icon</i>
+              <h3>Host</h3>
+            </div>
+            <p>Alejandra</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css';
+import { RiSettings2Line } from "react-icons/ri";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="user-image">
+                <img src="images/pfp.png" className="user-profile-img" alt="Profile Picture" />
+            </div>
+            <div className="user-welcome">
+                <h2>Welcome</h2>
+                <h1>Ancor</h1>
+            </div>
+            <div className="user-settings">
+                <RiSettings2Line size={40} />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
