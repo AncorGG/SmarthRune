3c2cae42610ce978bcc3eb5af6e91492b254a2b9
AncorGG <ancorgarcia63@gmail.com>
2025-08-19 13:33:40 +0100

    Character Details Start

--- src/pages/characterDetail/CharacterDetail.jsx
+++ src/pages/characterDetail/CharacterDetail.jsx
@@ -1,5 +1,11 @@
+import { Progress } from "antd";
 import Footer from "../../components/footer/Footer";
 import './CharacterDetail.css';
+import { IoIosArrowBack } from "react-icons/io";
+import { MdAttachMoney } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+import { FiSun } from "react-icons/fi";
+
 
 function CharacterDetail() {
     return (
@@ -7,18 +13,64 @@ function CharacterDetail() {
             <div className="main-content">
 
                 <div className="c-header">
-                    <div className="c-back-button"></div>
-                    <div className="c-bizzum"></div>
+                    <div className="c-back-button"><IoIosArrowBack size={30} /></div>
+                    <div className="c-bizzum"><MdAttachMoney size={30} /></div>
                 </div>
 
                 <div className="c-image">
-                    <img src="/images/pfp.pn" alt="Profile Picture" />
+                    <img src="/images/pfp.png" alt="Profile Picture" />
                 </div>
 
                 <div className="c-info">
-                    <div className="c-info-top">
-                        <h1 className="c-name">Jack O' Blivion</h1>
-                        <h2 className="c-campaign">Mazmorreos casuales</h2>
+                    <h1 className="c-name">Jack O' Blivion</h1>
+                    <h2 className="c-campaign">Mazmorreos casuales</h2>
+                    <hr className="c-hr" />
+                    <div className="c-info-bot">
+                        <div>
+                            <IoLocationOutline size={25} color="#D6202E" />
+                            <p>Contraviento, Livadi</p>
+                        </div>
+                        <div>
+                            <FiSun size={25} color="#D6202E" />
+                            <p>AE 24</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="c-level">
+                    <div className="c-level-info">
+                        <h3>Level 7 | Nihility Cleric</h3>
+                        <h3>67%</h3>
+                        <div className="progress-container">
+                            <Progress
+                                percent={67}
+                                showInfo={false}
+                                strokeColor={{
+                                    '0%': '#f63c3c',
+                                    '50%': '#ff5e9e',
+                                }}
+                                size={[350, 20]}
+                                trailColor="#e7e7e7"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="c-stats">
+                    <hr className="c-vr" />
+                    <div className="c-stat">
+                        <h3>HP</h3>
+                        <p>26</p>
+                    </div>
+                    <hr className="c-vr" />
+                    <div className="c-stat">
+                        <h3>Speed</h3>
+                        <p>30</p>
+                    </div>
+                    <hr className="c-vr" />
+                    <div className="c-stat">
+                        <h3>AC</h3>
+                        <p>14</p>
                     </div>
                 </div>
 
